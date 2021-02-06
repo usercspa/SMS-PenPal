a9126d9520cd8be98dfb8e910006714daa67a70f
usercspa <spacchbk@gmail.com>
2021-02-06 17:07:43 +0000

    sign up

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ bodyParser = require('body-parser'); //for receiving text messages
 var Firebase = require('firebase'), //store user's data
 usersRef = new Firebase('https://smspenpal-default-rtdb.firebaseio.com/Users/');
 
+//add numbers
+var numbers = [];
+usersRef.on('child_added', function(snapshot) {
+numbers.push( snapshot.val() );
+  console.log( 'Added number ' + snapshot.val() );
+})
+
+//sign up
+app.post('/message', function (req, res) {
+  var resp = new MessagingResponse();
+  if( req.body.Body.trim().toLowerCase() === 'yes' ) {
+    var fromNum = req.body.From;
+    if(numbers.indexOf(fromNum) !== -1) {
+      resp.message('You have signed up to SMS penpal!');
+    } else {
+      resp.message('Thank you, you have signed up to SMS penpal. Reply "STOP" to cancel.');
+      usersRef.push(fromNum);
+    }
+  } else {
+    resp.message('Welcome to SMS penpal. Type "yes" if you would like to sign up for SMS penpal.');
+  }
+
+  res.writeHead(200, {
+    'Content-Type':'text/xml'
+  });
+  res.end(resp.toString());
+
+});
+
 //send response
 var MessagingResponse = require('twilio').twiml.MessagingResponse;
 app = express();
